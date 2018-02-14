0921352738a911ef1f1671dd2c99c0690ab33c81
Adam R. McConnell <remelox@remelox.com>
2018-02-13 20:37:53 -0600

    Added IndexKey to blackboard

--- Source/ProvingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/ProvingGrounds/Private/ChooseNextWaypoint.cpp
@@ -2,9 +2,12 @@
 
 //#include "ChooseNextWaypoint.h"
 #include "../Public/ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
-	UE_LOG(LogTemp,Warning,TEXT("That thing."))
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName); 
+	UE_LOG(LogTemp,Warning,TEXT("Waypoint index: %i"), Index)
 	return EBTNodeResult::Succeeded;
 }
--- Source/ProvingGrounds/Public/ChooseNextWaypoint.h
+++ Source/ProvingGrounds/Public/ChooseNextWaypoint.h
@@ -13,8 +13,9 @@ UCLASS()
 class PROVINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
-	
-	
-	
 		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector IndexKey;
 };
